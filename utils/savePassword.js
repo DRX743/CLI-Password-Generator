4699921cc0976d109bd604112a835a7bf6ab964d
RedaMeziouni <meziounidb@gmail.com>
2022-06-18 11:24:25 +0100

    Save password func

---
+++ utils/savePassword.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const chalk = require('chalk');
+
+
+const savePassword = (password) => {
+    fs.open(path.join(__dirname, '../', 'password.txt'), 'a', 666, (e, id) => {
+        fs.write(id, password + os.EOL, numm, 'utf-8', () => {
+            fs.close(id, () => {
+                console.log(chalk.green('Password saved !'));
+            })
+        })
+    })
+}
+
+module.exports = savePassword;
